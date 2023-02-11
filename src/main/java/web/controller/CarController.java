364e19242901230bb43957da7e2fa6282dd62513
Vlad Tkachenko <tkachenko.vladik88@gmail.com>
2023-02-11 20:47:15 +0300

    checked

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -12,6 +12,7 @@ import web.service.CarService;
 public class CarController {
     private final CarService carService;
 
+
     public CarController(CarService carService) {
 
         this.carService = carService;
