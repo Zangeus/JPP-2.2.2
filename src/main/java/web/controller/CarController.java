9bf72840b46ab93bce4eae7fcc67c2a4fa005707
Vlad Tkachenko <tkachenko.vladik88@gmail.com>
2023-02-11 20:48:20 +0300

    checked and roll backed

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -12,7 +12,6 @@ import web.service.CarService;
 public class CarController {
     private final CarService carService;
 
-
     public CarController(CarService carService) {
 
         this.carService = carService;
